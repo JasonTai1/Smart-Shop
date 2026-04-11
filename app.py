b0d9771ad63cd9a1d371ff83be35da54efa9a13e
Bosco <boscotohzanjun@gmail.com>
2026-04-11 11:23:31 +0800

    complete login register system

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+from flask import Flask, render_template, request, redirect, session
 import sqlite3
 
+app = Flask(__name__)
+app.secret_key = "secret123"
+
+# Database
 def init_db():
     conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
@@ -9,43 +14,92 @@ def init_db():
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT,
         email TEXT,
-        password TEXT
+        password TEXT,
+        role TEXT,
+        location TEXT
     )
     """)
 
     conn.commit()
     conn.close()
-from flask import Flask, render_template, request
 
-app = Flask(__name__)
-
+# Home go login
 @app.route("/")
 def home():
-    return "Welcome to Smart Shop"
+    return redirect("/login")
 
+# Register
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+
         username = request.form["username"]
         email = request.form["email"]
         password = request.form["password"]
-        
+        role = request.form["role"]
+        location = request.form["location"]
+
         conn = sqlite3.connect("database.db")
         cursor = conn.cursor()
 
         cursor.execute(
-            "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
-            (username, email, password)
-         )
+            "INSERT INTO users (username, email, password, role, location) VALUES (?, ?, ?, ?, ?)",
+            (username, email, password, role, location)
+        )
 
         conn.commit()
         conn.close()
 
-        return "Register Success!"
+        return redirect("/login")
 
     return render_template("register.html")
 
+#  Login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE email=? AND password=?",
+            (email, password)
+        )
+
+        user = cursor.fetchone()
+        conn.close()
+
+        if user:
+            session["user_id"] = user[0]
+            session["email"] = user[2]
+            session["role"] = user[4]
+
+            return redirect("/profile")
+
+        else:
+            return "Invalid Email or Password"
+
+    return render_template("login.html")
+
+# Profile
+@app.route("/profile")
+def profile():
+    if "user_id" not in session:
+        return redirect("/login")
+
+    return render_template("profile.html", email=session["email"], role=session["role"])
+
+# Logout
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/login")
+
+# Run
 if __name__ == "__main__":
     init_db()
     app.run(debug=True)
-
