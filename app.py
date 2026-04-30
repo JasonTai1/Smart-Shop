6919726fcc9de08a00be399af3ac71509addf8e2
TAI TECK HWA <jasontath@gmail.com>
2026-04-30 12:37:06 +0800

    Finish navigation bar css,catogory navigation ,homepage banner,and example product card

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask , render_template
 from routes.main import main
 
 app = Flask(__name__)
 app.register_blueprint(main)
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
--- static/js/java.js
+++ static/js/java.js
@@ -1,3 +1,38 @@
+/* Morphing Dropdown Logic */
+const nav = document.getElementById('navbar');
+const menuContents = document.querySelectorAll('.menu-content');
+let closeTimeout;
+
+function openMenu(event,menuId) {
+
+  clearTimeout(closeTimeout); 
+  
+
+  menuContents.forEach(el => el.style.display = 'none');
+  
+
+  document.querySelectorAll('.nav-links a').forEach(el => el.parentElement.classList.remove('active-link'));
+  
+
+  const targetMenu = document.getElementById('menu-' + menuId);
+  if (targetMenu) {
+    targetMenu.style.display = 'block';
+    nav.classList.add('expanded');
+    
+
+    event.target.closest('li').classList.add('active-link');
+  }
+}
+
+function closeMenu() {
+
+  closeTimeout = setTimeout(() => {
+    nav.classList.remove('expanded');
+    document.querySelectorAll('.nav-links a').forEach(el => el.parentElement.classList.remove('active-link'));
+  }, 150);
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const track = document.getElementById("marqueeTrack");
 
